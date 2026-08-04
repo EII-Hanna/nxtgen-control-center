da11733f046c4cb4103259b187dc0a12a7ac319d
EII-Hanna <admin@nxtgendigital.de>
2026-08-04 19:47:22 +0200

    feat: load modern value story engine

--- nxtgen-data.js
+++ nxtgen-data.js
@@ -25,6 +25,7 @@
   loadAsset('style','./fulfillment-board.css');
   loadAsset('style','./automation-execution-ui.css');
   loadAsset('style','./weekly-accountability.css');
+  loadAsset('style','./value-story.css');
   loadAsset('script','./lead-crm.js');
   loadAsset('script','./offer-builder.js');
   loadAsset('script','./integrations.js');
@@ -39,6 +40,7 @@
   loadAsset('script','./fulfillment-board.js');
   loadAsset('script','./automation-execution-ui.js');
   loadAsset('script','./weekly-accountability.js');
+  loadAsset('script','./value-story.js');
 
   async function loadDashboard(){
     const db=window.NXTGEN_DB, org=window.NXTGEN_ORG_ID; if(!db||!org)return;
