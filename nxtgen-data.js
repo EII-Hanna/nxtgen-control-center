22a92a2bec14d62c7cef13b8437a9dad50540f39
EII-Hanna <admin@nxtgendigital.de>
2026-08-04 20:20:08 +0200

    Load payment automation cockpit

--- nxtgen-data.js
+++ nxtgen-data.js
@@ -29,6 +29,7 @@
   loadAsset('style','./renewal-expansion.css');
   loadAsset('style','./backoffice.css');
   loadAsset('style','./commercial-finance.css');
+  loadAsset('style','./payment-automation.css');
   loadAsset('script','./lead-crm.js');
   loadAsset('script','./offer-builder.js');
   loadAsset('script','./integrations.js');
@@ -47,6 +48,7 @@
   loadAsset('script','./renewal-expansion.js');
   loadAsset('script','./backoffice.js');
   loadAsset('script','./commercial-finance.js');
+  loadAsset('script','./payment-automation.js');
 
   async function loadDashboard(){
     const db=window.NXTGEN_DB, org=window.NXTGEN_ORG_ID; if(!db||!org)return;
